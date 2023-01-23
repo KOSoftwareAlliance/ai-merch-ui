33862eeb1827769c48eaaac7b04e15bdcd579418
Artur Socha <artur.socha00@gmail.com>
2023-01-23 22:43:13 +0100

    initial styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="page-wrapper">
+	<slot />
+</div>
+
+<style>
+	.page-wrapper{
+		padding: 5rem 10rem;
+		background-color: lightblue;
+		height: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Fileupload, Button } from 'flowbite-svelte'
+
+  let files: FileList;
+
+  const handleSubmit = () => {
+    console.log('upload')
+  }
+</script>
+
+<form on:submit={handleSubmit} class="upload-image">
+  <Fileupload name=image type='file' placeholder='Pick you image' inputClass="!p-0" bind:files />
+  <Button type="submit" class="w-48 -ml-3">Wyślij zdjęcie</Button>
+</form>
+
+<style lang="scss">
+  .upload-image{
+    display: flex;
+  }
+</style>
