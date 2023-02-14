cc80cb8328860f2e08829e22ed552d92cf475059
Artur Socha <artur.socha00@gmail.com>
2023-02-14 21:42:19 +0100

    upload styling, manage image template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Fileupload, Button, Spinner } from 'flowbite-svelte'
+  import { Dropzone, Button, Spinner } from 'flowbite-svelte'
 	import { uploadToFlaskServer } from '../ai-api';
 
   let files: FileList;
@@ -17,20 +17,31 @@
   };
 </script>
 
-<form class="upload-image">
-  <Fileupload name=image type='file' placeholder='Pick you image' inputClass="!p-0" bind:files />
-  <Button type="submit" class="w-36 mt-3 mx-auto" on:click={handleUpload}>Wyślij zdjęcie</Button>
+<form class="flex flex-col items-center">
+  <Dropzone id='dropzone' class="border-0" bind:files>
+    {#if !files}
+      <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+      <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Wybierz zdjęcie</span> lub przeciągnij je tutaj</p>
+    {:else}
+      <img src={URL.createObjectURL(files[0])} alt="Selected" class="w-full h-auto rounded-lg" />
+    {/if}
+  </Dropzone>
+
+  <Button pill type="submit" class="w-36 mt-3 h-36 mx-auto mb-4" on:click={handleUpload}>Wyślij zdjęcie</Button>
+
   {#if !imageUrl && isLoading}
     <Spinner />
-  {/if }
-  {#if imageUrl}
-    <img src={imageUrl} alt="Uploaded" />
+  {:else if imageUrl && !isLoading}
+    <div class="grid grid-cols-2 grid-rows-2 gap-2">
+      {#each [1, 2, 3, 4] as image (image)}
+        <a href="/manage-image?{imageUrl}">
+          <img src={imageUrl} alt="Uploaded" class="cursor-pointer" />
+        </a>
+      {/each} 
+    </div>
   {/if }
 </form>
 
 <style lang="scss">
-  .upload-image{
-    display: flex;
-    flex-direction: column;
-  }
+
 </style>
---
+++ src/routes/manage-image/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+
+  let imageUrl: string;
+  page.subscribe(({url}) => {
+    imageUrl = url.search.split('?')[1];
+  })
+</script>
+
+<div class="grid grid-cols-4 w-100 gap-3">
+  <img src={imageUrl} alt="Uploaded" class="col-span-3 rounded-lg" />
+  <div class="flex flex-col justify-around h-100">
+    <div class="border rounded-sm p-1 h-full text-center">
+      pr1
+    </div>
+    <div class="border rounded-sm p-1 h-full text-center">
+      pr2
+    </div>
+    <div class="border rounded-sm p-1 h-full text-center">
+      pr3
+    </div>
+  </div>
+</div>
