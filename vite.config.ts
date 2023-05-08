fa4b6ca21a98b831529b1968eb3f54c2f0372651
Artur Socha <artur.socha00@gmail.com>
2023-05-08 22:33:16 +0200

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import ssr from 'vite-plugin-ssr/plugin';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+	base: '/',
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	plugins: [sveltekit(), ssr({ target: 'node' })],
