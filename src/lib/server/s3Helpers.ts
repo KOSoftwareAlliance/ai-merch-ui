a8c584502196ab8e73b7fbbbed4a408cd8b85823
Artur Socha <artur.socha00@gmail.com>
2023-05-08 21:40:38 +0200

    env names changes

--- src/lib/server/s3Helpers.ts
+++ src/lib/server/s3Helpers.ts
@@ -1,9 +1,9 @@
-import { AWS_KEY_ID, AWS_REG, AWS_SECRET_KEY, S3_BUCKET_NAME } from "$env/static/private";
+import { KEY_ID, REGION, SECRET_KEY, S3_BUCKET_NAME } from "$env/static/private";
 import AWS from "aws-sdk";
 import { randomUUID } from 'crypto';
 
 export const createPresignedUrlWithClient = async () => {
-  AWS.config.update({ credentials: { accessKeyId: AWS_KEY_ID, secretAccessKey: AWS_SECRET_KEY }, region: AWS_REG });
+  AWS.config.update({ credentials: { accessKeyId: KEY_ID, secretAccessKey: SECRET_KEY }, region: REGION });
   const s3 = new AWS.S3();
   const key = `ai-merch-${randomUUID()}.jpg`
   const command = ({ Bucket: S3_BUCKET_NAME, Key: key, Expires: 3600 });
@@ -11,7 +11,7 @@ export const createPresignedUrlWithClient = async () => {
 };
 
 export const getObjectUrl = async (imageID: string) => {
-  AWS.config.update({ credentials: { accessKeyId: AWS_KEY_ID, secretAccessKey: AWS_SECRET_KEY }, region: AWS_REG });
+  AWS.config.update({ credentials: { accessKeyId: KEY_ID, secretAccessKey: SECRET_KEY }, region: REGION });
   const s3 = new AWS.S3();
   const key = `ai-merch-${imageID}.jpg`
   const command = ({ Bucket: S3_BUCKET_NAME, Key: key, Expires: 3600 });
