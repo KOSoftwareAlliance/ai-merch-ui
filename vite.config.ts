40bf883b67d7dcf3bdba1db6acbac9078f857de0
Artur Socha <artur.socha00@gmail.com>
2023-05-08 22:20:19 +0200

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import ssr from 'vite-plugin-ssr/plugin';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	plugins: [sveltekit(), ssr({ target: 'node' })],
+	optimizeDeps: {
+		exclude: ['aws-sdk']
+	}
 };
 
 export default config;
